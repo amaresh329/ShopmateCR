bc5a94e4e2f355ed879ea3c7e6b5e192dfaa657b
amaresh329 <vutukuriamaresh930@gmail.com>
2025-05-15 12:04:46 +0530

    Methods added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,10 +6,7 @@ import { CartCard } from "../components";
 export const Cart = () => {
   useTitle("Cart");
   const {total,cartList}=useCart()
-  //  const products = [
-  //   {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png",}, 
-  //   {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png",}, 
-  // ]
+ 
 
   return (
     <main>
